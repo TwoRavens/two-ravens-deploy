f62dae3f5d55207dc7c73f512d04f209417d05f4
Raman Prasad <raman_prasad@harvard.edu>
2020-01-16 09:02:37 -0500

    updated k8s

--- gce_2ravens/rendered/apply_all.sh
+++ gce_2ravens/rendered/apply_all.sh
@@ -1,7 +1,7 @@
-# Delete all Two Ravens pods
-kubectl apply -f dm_2ravens1_2020_0116.yaml
+# apply all Two Ravens pods
+kubectl apply -f dm_red-2ravens_2020_0116.yaml
 kubectl apply -f dm_blue-2ravens_2020_0116.yaml
-kubectl apply -f dm_2ravens3_2020_0116.yaml
-kubectl apply -f dm_2ravens4_2020_0116.yaml
-kubectl apply -f dm_2ravens5_2020_0116.yaml
-kubectl apply -f dm_2ravens6_2020_0116.yaml
+kubectl apply -f dm_lime-2ravens_2020_0116.yaml
+kubectl apply -f dm_maroon-2ravens_2020_0116.yaml
+kubectl apply -f dm_white-2ravens_2020_0116.yaml
+kubectl apply -f dm_orange-2ravens_2020_0116.yaml
--- gce_2ravens/rendered/delete_all.sh
+++ gce_2ravens/rendered/delete_all.sh
@@ -1,7 +1,7 @@
 # Delete all Two Ravens pods
-kubectl delete -f dm_2ravens1_2020_0116.yaml --grace-period=0 --force
+kubectl delete -f dm_red-2ravens_2020_0116.yaml --grace-period=0 --force
 kubectl delete -f dm_blue-2ravens_2020_0116.yaml --grace-period=0 --force
-kubectl delete -f dm_2ravens3_2020_0116.yaml --grace-period=0 --force
-kubectl delete -f dm_2ravens4_2020_0116.yaml --grace-period=0 --force
-kubectl delete -f dm_2ravens5_2020_0116.yaml --grace-period=0 --force
-kubectl delete -f dm_2ravens6_2020_0116.yaml --grace-period=0 --force
+kubectl delete -f dm_lime-2ravens_2020_0116.yaml --grace-period=0 --force
+kubectl delete -f dm_maroon-2ravens_2020_0116.yaml --grace-period=0 --force
+kubectl delete -f dm_white-2ravens_2020_0116.yaml --grace-period=0 --force
+kubectl delete -f dm_orange-2ravens_2020_0116.yaml --grace-period=0 --force
