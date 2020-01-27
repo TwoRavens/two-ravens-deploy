88feea52aa88ceddc7b5fcedc3ade45017b05f9f
Raman Prasad <raman_prasad@harvard.edu>
2020-01-27 15:27:40 -0500

    updated

--- gce_2ravens/rendered/delete_gce_apricot-blue-cyan.sh
+++ gce_2ravens/rendered/delete_gce_apricot-blue-cyan.sh
@@ -1,4 +1,4 @@
 # apply all Two Ravens pods
-kubectl delete -f gce_apricot_2020_0126.yaml --grace-period=0 --force
-kubectl delete -f gce_blue_2020_0126.yaml --grace-period=0 --force
-kubectl delete -f gce_cyan_2020_0126.yaml --grace-period=0 --force
+kubectl delete -f gce_apricot_2020_0127.yaml --grace-period=0 --force
+kubectl delete -f gce_blue_2020_0127.yaml --grace-period=0 --force
+kubectl delete -f gce_cyan_2020_0127.yaml --grace-period=0 --force
