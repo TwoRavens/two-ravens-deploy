c3e1bc5c8d8832dab90909e66915f95cc8786b76
Raman Prasad <raman_prasad@harvard.edu>
2020-12-02 14:35:58 -0500

    updated sizing

--- gce_2ravens/config_specs2.py
+++ gce_2ravens/config_specs2.py
@@ -218,13 +218,13 @@ gce_demo_site = dict(base_spec_01, **dict(\
     DEMO_AUTO_LOGIN="True",
     #
     #ta2_resources=['20000Mi', '25000Mi', '6000m', '8000m'],
-    ta3_resources=['100Mi', '300Mi', '100m', '150m'],
-    celery_resources=['450Mi', '600Mi', '200m', '200m'],
-    rook_resources=['100Mi', '200Mi', '100m', '100m'],
-    mongo_resources=['100Mi', '200Mi', '100m', '100m'],
-    redis_resources=['50Mi', '100Mi', '50m', '50m'],
-    nginx_resources=['50Mi', '100Mi', '50m', '50m'],
-    postgres_resources=['100Mi', '200Mi', '100m', '100m'],
+    nginx_resources=['256Mi', '500Mi', '500m', '500m'],
+    ta3_resources=['1000Mi', '2000Mi', '1000m', '1000m'],
+    celery_resources=['4500Mi', '6000Mi', '2000m', '1500m'],
+    rook_resources=['1000Mi', '1000Mi', '800m', '800m'],
+    mongo_resources=['1000Mi', '1000Mi', '500m', '1000m'],
+    redis_resources=['500Mi', '500Mi', '500m', '500m'],
+    postgres_resources=['500Mi', '1000Mi', '500m', '500m'],
     #
     ))
 
